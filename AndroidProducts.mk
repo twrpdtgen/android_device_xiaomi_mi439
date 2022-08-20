64489cfe4d86dd2c35010298a986dc43c08f159f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-20 03:46:42 +0200

    mi439: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: SDM439 Script version: 2.4.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_mi439.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_mi439-user \
+    omni_mi439-userdebug \
+    omni_mi439-eng
